3ff7b6d302d868dfe1ae48ffa19c505f6e62e0e6
André Paes <andrenet11@gmail.com>
2020-03-19 16:11:00 -0300

    changing test params

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :api_banking, ApiBanking.Repo,
   username: "andre_paes",
   password: "postgres",
   database: "api_banking_test",
-  hostname: "db",
+  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
